c4eaa8152cdc3e2ded5d7e10ef07b04fc8236b5a
DESKTOP-BDLBEKN\maja <mmihojevic94@gmail.com>
2017-12-26 11:53:13 +0100

    Edit css

--- public/javascripts/helper.js
+++ public/javascripts/helper.js
@@ -1,46 +1,11 @@
-/*
-$(document).ready(function() {
-    $('#prijavaDugme').click(function (event) {
-        $('#prijavaModal').modal('show')
-    });
-});
-$(document).ready(function() {
-    $('#dodajKursDugme').click(function (event) {
-        $('#dodajKursModal').modal('show')
-    });
-});*/
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
-    });
-});/*
-$(document).ready(function () {
-    $("#dodajKurs").on('click', function () {
-        $(".cover").fadeIn('slow');
-        $(".popup").fadeIn('slow');
+        $("#test").toggle();
+        $('#profilnaSlika').toggleClass('sidebarSmallIcon');
+        $('#nav').toggleClass('wide');
+        $('#tabela').toggleClass('wide');
+        $('#dashboard').toggleClass('wide');
 
-    });
-    $(".popup").on('click', function () {
-        if($(event.target).is("#close")){
-            $(".cover").fadeOut('slow');
-            $(".popup").fadeOut('slow');
-        }
-    });
-    $(".cover").on('click', function () {
-        $(".cover").fadeOut('slow');
-        $(".popup").fadeOut('slow');
     });
 });
-/*$(document).ready(function(){
-    $("#allC").click(function(){
-        $.get("/users/sviKursevi")
-            .done(function (data) {
-                if (data.status == 200) {
-                    $("#tabela").html();
-                }else{
-                    alert('Greska!');
-                }
-
-            });
-    });
-});*/
